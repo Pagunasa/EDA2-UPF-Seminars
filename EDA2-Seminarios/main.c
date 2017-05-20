e8d354baf927d511f6c10281b27f48c909fc8341
Pagunasa <Pagunasa@DESKTOP-DGB2C4I>
2017-05-20 15:18:28 +0200

    Seminar 3 EDA-2 attempt 1

--- EDA2-Seminarios/main.c
+++ EDA2-Seminarios/main.c
@@ -9,7 +9,7 @@
 #include <stdlib.h>
 
 int cmb_Din(int n, int k) {
-    int **combNumbers, i = 0, j;
+    int **combNumbers, i = 0, j, col, fil;
 
     combNumbers = malloc(n * sizeof (int *));
 
@@ -23,28 +23,42 @@ int cmb_Din(int n, int k) {
 
         for (i = 0; i < n; i++) {
             combNumbers[i][0] = 1;
-        }
 
-        for (i = 1; i < n; i++) {
+        }
+        for (i = 0; i < n; i++) {
             for (j = 1; j < k; j++) {
                 if (i == j) {
                     combNumbers[i][j] = 1;
+                } else if (j > i) {
+                    combNumbers[i][j] = 0;
                 }
+                combNumbers[0][j] = 1;
             }
         }
 
-        for (i = 1; i < n; i++) {
+        for (i = 2; i < n; i++) {
             for (j = 1; j < k; j++) {
-                combNumbers[i][j] = combNumbers[i - 1][j - 1] + combNumbers[i - 1][j];
+                col = i - 1;
+                fil = j - 1;
+                combNumbers[i][j] = combNumbers[col][fil] + combNumbers[col][j];
             }
         }
     }
-    return combNumbers[n-1][k-1];
+    col = i - 1;
+    fil = j - 1;
+    return combNumbers[col][fil];
 }
 
 int main(int argc, char** argv) {
-    int result;
-    result = cmb_Din(3, 4);
+    int result, N, K;
+    
+    printf("Introduce the numbers for combinatory: ");
+    scanf("%i %i", &N, &K);
+    
+    N = N+1;
+    K = K+1;
+    
+    result = cmb_Din(N,K);
     printf("%i", result);
     return (EXIT_SUCCESS);
 }
